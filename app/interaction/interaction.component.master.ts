a357df555874ce223effb4cdcf1a3a3fc82b6bab
Rohit Balan <rohitbalan@gmail.com>
2016-09-18 15:31:02 -0400

    not important

--- app/interaction/interaction.component.master.ts
+++ app/interaction/interaction.component.master.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
-import { Person }  from './interaction.person';
+import { Person } from './interaction.person';
 
 @Component({
-    selector: 'interaction-master',
-    templateUrl: './app/interaction/interaction.component.master.html'
+	selector: 'interaction-master',
+	templateUrl: './app/interaction/interaction.component.master.html'
 })
 export class MasterComponent {
-    master: string = 'Chutiya Master';
-    students: Person[] = [
-        {id: 1, name: 'Mr. IQ'},
-        {id: 2, name: 'Magneta'},
-        {id: 3, name: 'Bombasto'}];
+	master: string = 'Chutiya Master';
+	students: Person[] = [
+		{ id: 1, name: 'Mr. IQ' },
+		{ id: 2, name: 'Magneta' },
+		{ id: 3, name: 'Bombasto' }];
 }
--- app/interaction/interaction.module.ts
+++ app/interaction/interaction.module.ts
@@ -1,13 +1,13 @@
-import { NgModule }      from '@angular/core';
-import { CommonModule }      from '@angular/common';
-import { MasterComponent }  from './interaction.component.master';
-import { StudentComponent }  from './interaction.component.student';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MasterComponent } from './interaction.component.master';
+import { StudentComponent } from './interaction.component.student';
 
 @NgModule({
-    imports: [ CommonModule ],
-    declarations: [ MasterComponent, StudentComponent ],
-    exports: [ MasterComponent ]
+    imports: [CommonModule],
+    declarations: [MasterComponent, StudentComponent],
+    exports: [MasterComponent]
 })
 export class InteractionModule {
-    
+
 }
